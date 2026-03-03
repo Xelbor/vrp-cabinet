bd8389d2402581aad26ba3674f85d4b69986bbe9
Xelbor <xelbor45hail@gmail.com>
2026-03-03 17:31:44 +0500

    test commit

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -11,6 +11,11 @@ import { fetchAPI } from "@/lib/services/fetchAPI";
 import { formatDate, getTimeLeft } from '@/lib/date';
 import { useEffect, useState } from 'react';
 
+import { Skeleton } from "@/components/ui/skeleton";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function HomePageClient() {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState<any>(null);
@@ -104,10 +109,36 @@ export default function HomePageClient() {
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             <div className="grid grid-cols-2 gap-4">
               <motion.div whileHover={{ scale: 1.02 }}>
-                 <BalanceCard
-                    balance={data?.balance}
-                    isLoading={isLoading}
-                  />
+                 <Card className="bg-zinc-900 border-zinc-800 shadow-xl rounded-2xl z-1">
+                    <CardHeader className="px-4">
+                      <CardTitle className="flex items-center justify-between text-white text-lg">
+                          Мой баланс
+                      </CardTitle>
+                      <div className="text-zinc-400"> 
+                        <div className='flex justify-between items-start'>
+                          <div className='flex flex-col px-1'>
+                            <span className="text-white text-4xl">
+                              {isLoading ? (
+                                <Skeleton className="h-10 w-24 rounded-md" />
+                              ) : (
+                                <>
+                                  {data?.balance}
+                                  <span className='font-mono font-bold text-3xl'>₽</span>
+                                </>
+                              )}
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    </CardHeader>
+                    <div className='px-2 '>
+                      <Link href="balance">
+                          <Button variant="secondary" className="w-full h-8 md:h-10 text-xs md:text-sm bg-zinc-800 hover:bg-zinc-700 cursor-pointer">
+                            Пополнить баланс
+                          </Button>
+                        </Link>
+                    </div>
+                </Card>
               </motion.div>
     
               <motion.div whileHover={{ scale: 1.02 }}>
