5955b4ed378167383db9b557773df1bb8bca90cd
Xelbor <xelbor45hail@gmail.com>
2026-03-03 12:32:51 +0500

    test commit

--- app/balance/BalancePageClient.tsx
+++ app/balance/BalancePageClient.tsx
@@ -34,7 +34,7 @@ export default function BalancePage() {
         setIsLoading(true);
         
         if (!token) return;
-        const result = await fetchBalance(userId);
+        const result = await fetchBalance(token);
 
         setData({
           ...result
