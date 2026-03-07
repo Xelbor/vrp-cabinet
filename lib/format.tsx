bef2b1fab28da7dc45bd639a03fe1dca93aea788
Xelbor <xelbor45hail@gmail.com>
2026-03-07 14:33:08 +0500

    Fix traffic limit

--- lib/format.tsx
+++ lib/format.tsx
@@ -3,7 +3,9 @@
 import { DiApple, DiAndroid, DiWindows, DiLinux } from "react-icons/di";
 
 export const bytesToGiB = (value: number): string => {
-  return (value / (1024 ** 3)).toFixed(2)
+  if (value === 0) return '∞';
+  
+  return (value / (1024 ** 3)).toFixed(2);
 }
 
 export function definePlatformIcon(platform: string) {
